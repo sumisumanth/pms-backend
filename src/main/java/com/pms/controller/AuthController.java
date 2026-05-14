2c3b2039bcf17bf0308f53b48d5099c5c7a0dc13
sumisumanth <sumisumanth2003@gmail.com>
2026-05-15 04:12:18 +0530

    updated cors for deployment

--- src/main/java/com/pms/controller/AuthController.java
+++ src/main/java/com/pms/controller/AuthController.java
@@ -1,33 +1,35 @@
 package com.pms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pms.entity.User;
 import com.pms.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
-@Autowired
-private AuthService authService;
+    @Autowired
+    private AuthService authService;
 
+    /* REGISTER */
 
-/* REGISTER */
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return authService.register(user);
+    }
 
-@PostMapping("/register")
-public User register(@RequestBody User user){
-return authService.register(user);
-}
+    /* LOGIN */
 
-
-/* LOGIN */
-
-@PostMapping("/login")
-public User login(@RequestBody User loginRequest){
-return authService.login(loginRequest);
-}
+    @PostMapping("/login")
+    public User login(@RequestBody User loginRequest){
+        return authService.login(loginRequest);
+    }
 
 }
