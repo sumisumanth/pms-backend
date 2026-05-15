38399a07804557c7485125ae201254aab6ad0c72
sumisumanth <sumisumanth2003@gmail.com>
2026-05-15 14:59:47 +0530

    fixed cors issue

--- src/main/java/com/pms/config/SecurityConfig.java
+++ src/main/java/com/pms/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.pms.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,36 +18,59 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
-@Bean
-SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-http
-.csrf(csrf -> csrf.disable())
-.cors(cors -> cors.disable())
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
-.authorizeHttpRequests(auth -> auth
+            .authorizeHttpRequests(auth -> auth
 
-.requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/admin/**").permitAll()
+                .requestMatchers("/api/user/**").permitAll()
 
-.requestMatchers("/api/admin/**").permitAll()
+                .anyRequest().permitAll()
+            )
 
-.requestMatchers("/api/admin/products/**").permitAll()
+            .formLogin(form -> form.disable());
 
-.requestMatchers("/api/user/**").permitAll()
+        return http.build();
+    }
 
-.anyRequest().permitAll()
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
 
-)
+        CorsConfiguration configuration = new CorsConfiguration();
 
-.formLogin(form -> form.disable());
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://pms-frontend-indol-three.vercel.app"
+        ));
 
-return http.build();
+        configuration.setAllowedMethods(List.of(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ));
 
-}
+        configuration.setAllowedHeaders(List.of("*"));
 
-@Bean
-PasswordEncoder passwordEncoder(){
-return new BCryptPasswordEncoder();
-}
+        configuration.setAllowCredentials(true);
 
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
